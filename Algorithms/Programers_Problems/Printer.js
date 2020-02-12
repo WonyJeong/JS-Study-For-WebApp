4b517b65d296d9bc4aefe134e71428b61ab24a8a
WonyJeong <jwy27772415@gmail.com>
2020-02-12 23:54:11 +0900

    modity Printer.js
    modity Printer.js

--- Algorithms/Programers_Problems/Printer.js
+++ Algorithms/Programers_Problems/Printer.js
@@ -1,12 +1,42 @@
-const solution = (arr, location) => {
-  var data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  for (var i = 0; i < 11; i++) {
-    data[arr[i]]++;
+function solution(priorities, location) {
+  var i = 0;
+  var len = priorities.length;
+  var cursor = new Array(priorities.length);
+
+  cursor.fill(0);
+  cursor[location] = 1;
+
+  while (i !== priorities.length) {
+    const maxValue = Math.max(...priorities.slice(i, len));
+    if (priorities[i] < maxValue) {
+      var temp = priorities[i];
+      priorities.splice(i, 1);
+      priorities.push(temp);
+      var temp = cursor[i];
+      cursor.splice(i, 1);
+      cursor.push(temp);
+    } else {
+      i++;
+    }
   }
+  return cursor.indexOf(1) + 1;
+}
 
-  return 0;
-};
+console.log(solution([2, 1, 3, 2]), 2);
 
-const arr = [2, 1, 3, 2];
-const location = 2;
-console.log(solution(arr, location));
+function solution(priorities, location) {
+  var list = priorities.map((t, i) => ({
+    my: i === location,
+    val: t
+  }));
+  var count = 0;
+  while (true) {
+    var cur = list.splice(0, 1)[0];
+    if (list.some(t => t.val > cur.val)) {
+      list.push(cur);
+    } else {
+      count++;
+      if (cur.my) return count;
+    }
+  }
+}
