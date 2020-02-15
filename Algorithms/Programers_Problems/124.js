c0059b3c3de4f12a95de9554c6196ccaef5ac4e2
WonyJeong <jwy27772415@gmail.com>
2020-02-15 20:32:47 +0900

    solve 124
    solve 124

--- Algorithms/Programers_Problems/124.js
+++ Algorithms/Programers_Problems/124.js
@@ -2,15 +2,11 @@ function solution(n) {
   var answer = "";
   var num = n;
   while (num != 0) {
-    quo = Math.floor(num % 3);
+    var quo = Math.floor(num % 3);
     quo = quo === 0 ? 3 : quo;
     num = (num - quo) / 3;
-    lem = quo == 3 ? 4 : quo;
+    var lem = quo == 3 ? 4 : quo;
     answer = lem + answer;
   }
-  return answer;
-}
-
-for (var i = 1; i < 21; i++) {
-  console.log(i + " : " + solution(i));
+  return Number(answer);
 }
