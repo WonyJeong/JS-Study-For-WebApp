f7df267d1a74507888874ebbb6e7dd98c6ffd399
WonyJeong <jwy27772415@gmail.com>
2020-02-19 22:17:25 +0900

    Create mathsolve.js
    -

---
+++ Algorithms/Programers_Problems/mathsolve.js
@@ -0,0 +1,25 @@
+const scoring = (arr, answer) => {
+  var temp = 0;
+  const len = arr.length;
+  for (var i = 0; i < len; i++) if (answer[i] === arr[i % len]) temp++;
+  return temp;
+};
+
+const solution = answers => {
+  const arr = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+  ];
+  var answer = [];
+  for (var i = 0; i < 3; i++) {
+    const temp = arr[i];
+    answer.push(scoring(arr[i], answers));
+  }
+
+  return answer;
+};
+
+//const answers = [1, 2, 3, 4, 5];
+const answers = [1, 3, 2, 4, 2];
+console.log(solution(answers));
