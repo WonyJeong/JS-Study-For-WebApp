b6a4d749df5d47b548213e527ef91cdd39d159f8
WonyJeong <jwy27772415@gmail.com>
2020-02-27 15:24:06 +0900

    test
    -

--- Algorithms/Programers_Problems/FInd_MaxNumber.js
+++ Algorithms/Programers_Problems/FInd_MaxNumber.js
@@ -1,18 +1,56 @@
-const solution = numbers => {
-  var arr = new Array(10);
-  var nums = numbers.splice(val => String(val));
-  for (var i = 0; i < nums.length; i++) {
-    var index = nums[i];
-    console.log(index + 5);
-    //arr[index].push(nums[i]);
+const sortArray = (index, arr) => {
+    var str = "";
+    var find_Max_len = arr.map(function(str) {
+      return str.length;
+    })
+  
+    var maxlen = Math.max.apply(null,find_Max_len);
+    
+    var ith_Arr = [];
+    
+    var tempArr = arr;
+    for(var i =0; i< arr.length ; i++){
+      var itemLen = arr[i].length;
+      var tail = String(arr[i]);
+      while(itemLen < maxlen){
+          tail += String(index);
+      }
+      
+        var tempObj = {
+        realValue : 
+      }
+    }
+    
+    
+    console.log(find_Max_len);
+    return str;
   }
-
-  for (var i = 1; i < 10; i++) {
-    console.log(" i : " + i + " => " + nums[i]);
-  }
-
-  return nums;
-};
-
-const numbers = [3, 30, 34, 5, 9];
-console.log(solution(numbers));
+  
+  
+  const solution = numbers => {
+    //Allocation 2D Array & answer String
+    var answer = "";
+    var arr = [];
+    for (var i = 1; i < 10; i++){
+      arr[i] = new Array();
+    }
+    
+    //Set Numbers Tree
+    for(var i = 0; i<numbers.length; i++){
+      var temp = String(numbers[i]);
+      arr[temp[0] * 1].push(temp);  
+    }
+    
+    for(var i = 9; i > 0; i--){
+      if(arr[i].length !== 0){
+        answer += sortArray(i, arr[i]);
+      }
+    }
+  
+  
+    return 0;
+  };
+  
+  const numbers = [3, 30, 34, 5, 9];
+  console.log(solution(numbers));
+  
